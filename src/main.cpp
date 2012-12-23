34348b9cb289011b14e9fc27761943ce88e04490
Jason Van Cleave <jvcleave@gmail.com>
2012-12-23 15:39:38 -0500

    init

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include "ofMain.h"
+#include "testApp.h"
+#ifdef TARGET_OPENGLES
+#include "ofGLES2Renderer.h"
+#endif
+//========================================================================
+int main( ){
+
+	ofSetLogLevel(OF_LOG_VERBOSE);
+	#ifdef TARGET_OPENGLES
+	ofSetCurrentRenderer(ofPtr<ofBaseRenderer>(new ofGLES2Renderer()));
+	#endif
+	ofSetupOpenGL(1280,720, OF_WINDOW);			// <-------- setup the GL context
+
+	// this kicks off the running of my app
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
+	ofRunApp( new testApp());
+
+}
