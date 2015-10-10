f506879349fe5f9cac116d8fa74474607e784025
Jason Van Cleave <jvcleave@gmail.com>
2015-10-10 20:25:12 +0000

    ifdef for 09RC

--- example-shader/src/main.cpp
+++ example-shader/src/main.cpp
@@ -1,12 +1,23 @@
 #include "ofMain.h"
 #include "shaderApp.h"
-#include "ofGLProgrammableRenderer.h"
+#if (OF_VERSION_MINOR != 9)
+    #include "ofGLProgrammableRenderer.h"
+#endif
+
 
 int main()
 {
-	ofSetLogLevel(OF_LOG_VERBOSE);
-	ofSetLogLevel("ofThread", OF_LOG_ERROR);
-	ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
-	ofSetupOpenGL(1280, 720, OF_WINDOW);
-	ofRunApp( new shaderApp());
+    ofSetLogLevel(OF_LOG_VERBOSE);
+#if (OF_VERSION_MINOR == 9)
+    ofGLESWindowSettings settings;
+    settings.width = 1280;
+    settings.height = 720;
+    settings.setGLESVersion(2);
+    ofCreateWindow(settings);
+#else
+    ofSetLogLevel("ofThread", OF_LOG_ERROR);
+    ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
+    ofSetupOpenGL(1280, 720, OF_WINDOW);
+#endif
+    ofRunApp( new shaderApp());
 }
