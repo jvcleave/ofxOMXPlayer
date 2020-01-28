94178e69d2be5e7ba45c724e3c666009896a7ba1
Jason Van Cleave <jvcleave@gmail.com>
2020-01-28 01:20:14 -0500

    conditional for RPI4 - compiles

--- src/ofxOMXPlayerEngine.cpp
+++ src/ofxOMXPlayerEngine.cpp
@@ -494,6 +494,9 @@ void ofxOMXPlayerEngine::onUpdate(ofEventArgs& eventArgs)
 }
 
 #pragma mark EGLImage
+
+#if defined(TARGET_RASPBERRY_PI_LEGACY)
+
 bool ofxOMXPlayerEngine::generateEGLImage()
 {
     bool success = false;
@@ -645,7 +648,183 @@ void ofxOMXPlayerEngine::destroyEGLImage()
     }
     
 }
+#else
 
+static PFNEGLCREATEIMAGEKHRPROC createImageProc = NULL;
+EGLImageKHR createImage(EGLDisplay dpy, EGLContext ctx, EGLenum target,
+                        EGLClientBuffer buffer, const EGLint *attrib_list)
+{
+    if (!createImageProc)
+    {
+        createImageProc = (PFNEGLCREATEIMAGEKHRPROC)eglGetProcAddress("eglCreateImageKHR");
+    }
+    
+    return createImageProc(dpy, ctx, target, buffer, attrib_list);
+}
+static PFNEGLDESTROYIMAGEKHRPROC destroyImageProc = NULL;
+
+EGLBoolean destroyImage(EGLDisplay dpy, EGLImageKHR image)
+{
+    if (!destroyImageProc)
+    {
+        destroyImageProc = (PFNEGLDESTROYIMAGEKHRPROC)eglGetProcAddress("eglDestroyImageKHR");
+    }
+    
+    return destroyImageProc(dpy, image);
+}
+
+bool ofxOMXPlayerEngine::generateEGLImage()
+{
+    bool success = false;
+    bool needsRegeneration = false;
+    
+    if (!texture.isAllocated())
+    {
+        needsRegeneration = true;
+    }
+    else
+    {
+        if (texture.getWidth() != videoWidth && texture.getHeight() != videoHeight)
+        {
+            needsRegeneration = true;
+        }
+    }
+    
+    if (!fbo.isAllocated())
+    {
+        needsRegeneration = true;
+    }
+    else
+    {
+        if (fbo.getWidth() != videoWidth && fbo.getHeight() != videoHeight)
+        {
+            needsRegeneration = true;
+        }
+    }
+    
+    if(!needsRegeneration)
+    {
+        //ofLogVerbose(__func__) << "NO CHANGES NEEDED - RETURNING EARLY";
+        return true;
+    }
+    
+    if (appEGLWindow == NULL)
+    {
+        appEGLWindow = (ofAppEGLWindow *) ofGetWindowPtr();
+    }
+    
+    if (appEGLWindow == NULL)
+    {
+        ofLogError(__func__) << "appEGLWindow is NULL - RETURNING";
+        return false;
+    }
+    if (display == NULL)
+    {
+        display = appEGLWindow->getEglDisplay();
+    }
+    if (context == NULL)
+    {
+        context = appEGLWindow->getEglContext();
+    }
+    
+    if (display == NULL)
+    {
+        ofLogError(__func__) << "display is NULL - RETURNING";
+        return false;
+    }
+    if (context == NULL)
+    {
+        ofLogError(__func__) << "context is NULL - RETURNING";
+        return false;
+    }
+    
+    if (needsRegeneration)
+    {
+        
+        fbo.allocate(videoWidth, videoHeight, GL_RGBA);
+        texture.allocate(videoWidth, videoHeight, GL_RGBA);
+        texture.setTextureWrap(GL_REPEAT, GL_REPEAT);
+        textureID = texture.getTextureData().textureID;
+    }
+    
+    ofLog() << "textureID: " << textureID;
+    ofLog() << "tex.isAllocated(): " << texture.isAllocated();
+    ofLog() << "videoWidth: " << videoWidth;
+    ofLog() << "videoHeight: " << videoHeight;
+    ofLog() << "pixels: " << videoHeight;
+    
+    // setup first texture
+    int dataSize = videoWidth * videoHeight * 4;
+    
+    if (pixels && needsRegeneration)
+    {
+        delete[] pixels;
+        pixels = NULL;
+    }
+    
+    if (pixels == NULL)
+    {
+        pixels = new unsigned char[dataSize];
+    }
+    ofLog() << "dataSize: " << dataSize;
+    
+    //memset(pixels, 0xff, dataSize);  // white texture, opaque
+    
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, videoWidth, videoHeight, 0,
+                 GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+    
+    
+    if (eglImage && needsRegeneration)
+    {
+        destroyEGLImage();
+    }
+    
+    // Create EGL Image
+    eglImage = createImage(display, context, EGL_GL_TEXTURE_2D_KHR, (EGLClientBuffer)textureID, NULL);
+
+    if (eglImage == EGL_NO_IMAGE_KHR)
+    {
+        ofLog()    << "Create EGLImage FAIL <---------------- :(";
+        
+    }
+    else
+    {
+        success = true;
+        ofLog()  << "Create EGLImage PASS <---------------- :)";
+    }
+    return success;
+    
+}
+
+void ofxOMXPlayerEngine::destroyEGLImage()
+{
+    
+    
+    if (eglImage)
+    {
+        if (appEGLWindow == NULL)
+        {
+            appEGLWindow = (ofAppEGLWindow *) ofGetWindowPtr();
+        }
+        
+        if (display == NULL)
+        {
+            display = appEGLWindow->getEglDisplay();
+        }
+        
+        if (!destroyImage(display, eglImage))
+        {
+            ofLog() << __func__ << " FAIL <---------------- :(";
+        }else
+        {
+            ofLog() << __func__ << " PASS <---------------- :)";
+        }
+        eglImage = NULL;
+    }
+    
+}
+#endif
 
 #pragma mark THREAD
 
